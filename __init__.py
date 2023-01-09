88a6bf303df720648a7fb45f4479c30c6aee645e
Stanley7096 <865798919@qq.com>
2023-01-09 19:57:24 +0800

    leo-messi-skill-update
    mycroft-skill-update

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 
 class LeoMessi(MycroftSkill):
@@ -8,7 +9,18 @@ class LeoMessi(MycroftSkill):
     @intent_file_handler('messi.leo.intent')
     def handle_messi_leo(self, message):
         self.speak_dialog('messi.leo')
-
+    
+    @intent_file_handler('club.messi.intent')
+    def handle_club_messi(self, message):
+        self.speak_dialog('club')
+    
+    @intent_file_handler('golden.ball.intent')
+    def handle_golden_ball(self, message):
+        self.speak_dialog('golden.ball')
+        
+    @intent_handler(IntentBuilder('MessiIntent').require('MessiKeyword'))
+    def handle_Messi_Intent(self, message):
+    	self.speak_dialog('messi')
 
 def create_skill():
     return LeoMessi()
